b817e67e122a78d5f55601b8b0452e42c137bda8
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-02-24 14:03:06 -0300

    Created settings for connecting to the database.

---
+++ Configurations/DatabaseConfig.cs
@@ -0,0 +1,22 @@
+﻿using CarTest.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarTest.Configurations
+{
+    public static class DatabaseConfig
+    {
+        public static IServiceCollection AddDatabaseConfiguration
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["MSSQLServerSQLConnection:MSSQLServerSQLConnectionString"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("Connection string 'MSSQLServerSQLConnectionString' not found.");
+            }
+
+            services.AddDbContext<CarContext>(options =>
+                options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
